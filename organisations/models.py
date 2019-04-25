11c5ab2748ef705551e84ac09a2cbf981dc03583
djnsubhasish <subhasishlove1@gmail.com>
2019-04-25 20:26:52 +0530

    org_done_custreg_not_done

--- organisations/models.py
+++ organisations/models.py
@@ -1,9 +1,20 @@
 from django.db import models
 
-class organisations(models.Model):
-	orgid = models.CharField(max_length=3)
+class Organisations(models.Model):
 	orgname = models.CharField(max_length=50)
 	orgemail = models.CharField(max_length=100)
 	orgcc  = models.CharField(max_length=500)
 	orgsendername = models.CharField(max_length=50)
 	orgsenderphn =  models.CharField(max_length=13)
+	orgpassword =  models.CharField(max_length=13)
+class Meta:
+		db_table='organisations'
+
+class Customer(models.Model):
+	orgid = models.CharField(max_length=3)
+	custname = models.CharField(max_length=50)
+	custemail = models.CharField(max_length=100)
+	custphn = models.CharField(max_length=13)
+	custstatus = models.CharField(max_length=10)
+class Meta:
+	db_table='customer'
--- organisations/views.py
+++ organisations/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-
+from django.conf.urls.static import static
+from .models import Organisations , Customer
 
 # Create your views here.
 
@@ -9,15 +10,29 @@ def portfolio(request):
     return render(request,'organisations/portfolio.html')
 def orgreg(request):
 	if request.method == 'POST':
-		form.save()
-		#return redirect ('success')
+		
+		org_name=request.POST.get('orgname')
+		org_email=request.POST.get('orgemail')
+		org_cc=request.POST.get('orgcc')
+		org_sender_email=request.POST.get('orgsenderemail')
+		org_sender_phn=request.POST.get('orgsenderphn')
+		org_password=request.POST.get('orgpassword')
+		
+		c = Organisations(orgname = org_name,orgemail = org_email, orgcc=org_cc,orgsendername=org_sender_email, orgsenderphn=org_sender_phn, orgpassword=org_password)
+		c.save()
+		return render(request,'organisations/orgRegister.html')
 	else:
 
 		return render(request,'organisations/orgRegister.html')
 def custreg(request):
-	#if request.method == 'POST':
-	#	form.save()
-		return render(request,'organisations/customer.html')
-	#else:
-	#	return redirect('success')		
+	if request.method == 'POST':
+		
+		cust_id=request.POST.get('orgid')
+		cust_name=request.POST.get('custname')
+		cust_email=request.POST.get('custemail')
+		cust_phn=request.POST.get('custphone')
+		cust_status=request.POST.get('custstatus')
 
+		return render(request,'organisations/orgRegister.html')
+	else:
+		return render(request,'organisations/customer.html')
